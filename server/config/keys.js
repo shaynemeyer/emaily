289cebef5205eb053848438c209bf95517006951
Shayne Meyer <shayne.meyer@gmail.com>
2017-08-15 12:54:56 -0700

    added heroku prod config

---
+++ server/config/keys.js
@@ -0,0 +1,10 @@
+// keys.js
+if(process.env.NODE_ENV === 'production') {
+  // We are in production - return the prod set of keys
+  module.exports = require('./prod');
+
+} else {
+  // We are in development - return the dev keys!!!
+  module.exports = require('./dev');
+}
+
