2d532c48e15b6c972c8425411cfb2a35eb7ca5c7
Shayne Meyer <shayne.meyer@gmail.com>
2017-08-15 07:30:57 -0700

    fixed auth issue with mlab, added cookie code and serialize & deserialize

--- server/services/passport.js
+++ server/services/passport.js
@@ -1,6 +1,20 @@
 const passport = require('passport');
 const GoogleStrategy = require('passport-google-oauth20').Strategy;
+const mongoose = require('mongoose');
 const keys = require('../config/keys');
+
+const User = mongoose.model('users');
+
+passport.serializeUser((user, done) => {
+  done(null, user.id);
+});
+
+passport.deserializeUser((id, done) => {
+  User.findById(id)
+    .then(user => {
+      done(null, user);
+    });
+});
 
 passport.use(
   new GoogleStrategy({
@@ -9,8 +23,19 @@ passport.use(
       callbackURL: '/auth/google/callback'
     },
     (accessToken, refreshToken, profile, done) => {
-      console.log('accessToken', accessToken);
-      console.log('refreshToken', refreshToken);
-      console.log('profile', profile);
-    })
+      User.findOne({ googleId: profile.id })
+        .then((existingUser) => {
+          if (existingUser) {
+            // we already have a record with the given profile ID
+            console.log("user already exists", profile.id);
+            done(null, existingUser);
+          } else {
+            // we don't have a user with this id, make a new record.
+            new User({ googleId: profile.id })
+              .save()
+              .then(user=> done(null, user));
+          }
+        });
+    }
+  )
 );
