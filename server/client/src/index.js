57511886d7c33767aaa3b8462bff30a116c4ead3
Shayne Meyer <shayne.meyer@gmail.com>
2017-08-17 06:49:24 -0700

    added mock components

--- server/client/src/index.js
+++ server/client/src/index.js
@@ -1,4 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { Provider } from 'react-redux';
+import { createStore, applyMiddleware } from 'redux';
 
-ReactDOM.render();
+import App from './components/App';
+import reducers from './reducers';
+
+const store = createStore(reducers, {}, applyMiddleware());
+
+ReactDOM.render(
+  <Provider store={store}><App/></Provider>,
+  document.querySelector('#root')
+);
