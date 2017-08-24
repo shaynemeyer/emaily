04f88f977075257f54375ffd4725e9d3175a1fef
Shayne Meyer <shayne.meyer@gmail.com>
2017-08-24 08:13:21 -0700

    fixed form and redirect

--- server/client/src/components/surveys/SurveyNew.jsx
+++ server/client/src/components/surveys/SurveyNew.jsx
@@ -1,15 +1,30 @@
 // SurveyNew shows SurveyForm and SurveyFormReview
 import React, { Component } from 'react';
+import { reduxForm } from 'redux-form';
 import SurveyForm from './SurveyForm';
+import SurveyFormReview from './SurveyFormReview';
 
 class SurveyNew extends Component {
+  state = { showReview: false };
+
+  renderContent() {
+    if(this.state.showReview) {
+      return <SurveyFormReview onCancel={() => this.setState({ showReview: false })} />;
+    }
+
+    return <SurveyForm
+      onSurveySubmit={() => this.setState({ showReview: true })} />;
+  }
+
   render() {
     return (
       <div>
-        <SurveyForm/>
+        {this.renderContent()}
       </div>
     );
   }
 }
 
-export default SurveyNew;
+export default reduxForm({
+  form: 'surveyForm'
+})(SurveyNew);
